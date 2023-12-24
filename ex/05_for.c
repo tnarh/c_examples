d83aff39ee5f6f8608418ab13c01467d5dbfb188
hrant m <152485692+tnarh@users.noreply.github.com>
2023-12-24 15:30:56 +0400

    FOR

---
+++ ex/05_for.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+  for (int i = 0; i < 10; i++) {
+  /*
+  "for" is a type of loop where it goes through a list of numbers. In this situation, the variable "i" is the one that is going through the list, and the list goes from 0 to 9 (i = 0; i < 10)
+  */
+    printf("%d\n", i);
+  }
+  
+  return 0;
+}
+
+/*
+Output:
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+*/
