7ff8912200342824541c438f1111efb4d35bfb45
hrant m <152485692+tnarh@users.noreply.github.com>
2023-12-24 15:17:25 +0400

    SCANF

---
+++ ex/scanf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+  int n1 = 0; // Initializing a variable
+  
+  printf("Enter a number: ");
+  scanf("%d", &n1); // "scanf" waits for user input, with "&n1" representing the variable the users input will be saved to
+  
+  return 0;
+}
