de5c3ce85d47e23f988089479dadc018700f4a3d
hrant m <152485692+tnarh@users.noreply.github.com>
2024-02-11 10:50:32 +0400

    Add files via upload

---
+++ exercises/mean.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+double findMean(int arr[], int arrlen) {
+    int sum = 0;
+    for (int i = 0; i < arrlen; i++) {
+        sum += arr[i];
+    }
+    return (double)sum / arrlen;
+}
+
+int main() {
+    int arrlen = 5;
+    int arr[arrlen];
+
+    printf("Enter 5 numbers: ");
+    for (int i = 0; i < arrlen; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    double mean = findMean(arr, arrlen);
+
+    printf("Mean: %lf\n", mean);
+
+    return 0;
+}
+
